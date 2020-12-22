d2f2ca21ce6600ad0e57198bab08853c82f966db
Pancham Goyal <panchamgoyal@gmail.com>
2020-12-22 12:27:16 +0530

    inital commit

---
+++ springboot-crud-restful-webservices-master/src/main/java/com/tenant/springboot/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.tenant.springboot.config;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final long serialVersionUID = -7858869558953243875L;
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+				if(request.getRequestURL().equals("http://localhost:8080/swagger-ui.html"))
+				{
+					return;
+				}
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+	}
+}
